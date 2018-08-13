4e35e3dc669bd890faa4f5ab1d11d21a6a03e422
Tim Ramandt <tim.ramandt@hotmail.com>
2018-08-13 23:45:20 +0200

    data classes

---
+++ Data/DatabaseContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Data.Models;
+using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data
+{
+    public class DatabaseContext : DbContext
+    {
+        public DbSet<Boss> Bosses { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<DiscordUser> DiscordUsers { get; set; }
+        public DbSet<Session> Sessions { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //TO DO: put this in a config file
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=PvmScheduler;Trusted_Connection=True;");
+        }
+    }
+}
---
+++ Data/Models/Session.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Data.Models
+{
+    public class Session
+    {
+        [Key]
+        public int SessionId { get; set; }
+
+        public Boss Boss { get; set; }
+        public int Limit { get; set; }
+
+        public DiscordUser Host { get; set; }
+        public DateTime Time { get; set; }
+    }
+}
